e428a6db4c8a1ed93e6a13dfc1d1d15cbf76b7a5
Razul27 <mohamedrazul916@gmail.com>
2022-07-22 17:58:24 +0530

    New code

---
+++ src/org/phone/InternalStorage.java
@@ -0,0 +1,17 @@
+package org.phone;
+
+public class InternalStorage {
+private void processorName() {
+	System.out.println("Processor Name - IOS");
+}
+private void ramSize() {
+	System.out.println("Ram Size-6GB");
+}
+public static void main(String[] args) {
+	InternalStorage i=new InternalStorage();
+	i.processorName();
+	i.ramSize();
+	ExternalStorage e=new ExternalStorage();
+	e.size();
+}
+}
